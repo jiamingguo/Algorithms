833ce23d0266b534e20de2fad0867770fdbe7d5b
jiamingguo <gjm1993217@gmail.com>
2017-03-26 00:51:17 -0400

    update question 445

---
+++ src/Medium/AddTwoNumbersII_445/ListNode.java
@@ -0,0 +1,10 @@
+package Medium.AddTwoNumbersII_445;
+
+/**
+ * Created by gjm19_000 on 3/26/2017.
+ */
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
